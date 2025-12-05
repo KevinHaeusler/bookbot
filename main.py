6d7d49cd0ed47d97450a9cd9289934783f6417ad
KevinHaeusler <kevin@kh.dev>
2025-12-05 10:32:31 +0100

    L4: Count Words

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
-        print(file_contents)
+        return file_contents
+
+def count_words(text):
+    print(f"Found {len(text.split())} total words")
+    return len(text.split())
 
 def main():
-    get_book_text("books/frankenstein.txt")
+    count_words(get_book_text("books/frankenstein.txt"))
 
 main()
