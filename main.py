123717ca6610dc88aee9f82972b0001c3e6dbde1
KevinHaeusler <kevin@kh.dev>
2025-12-05 11:30:38 +0100

    CH3L1: Print a Report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_num_char
+from stats import get_num_words, get_num_char, chars_dict_to_sorted_list
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,7 +6,17 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
-    get_num_words(get_book_text("books/frankenstein.txt"))
     get_num_char(get_book_text("books/frankenstein.txt"))
-
+    sorted_list = chars_dict_to_sorted_list(get_num_char(get_book_text("books/frankenstein.txt")))
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f" Found {get_num_words(get_book_text("books/frankenstein.txt"))} total words")
+    print("--------- Character Count -------")
+    for item in sorted_list:
+        ch = item["char"]
+        count = item["num"]
+        if ch.isalpha():
+            print(f"{ch}: {count}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -1,7 +1,7 @@
 import string
 
 def get_num_words(text):
-    print(f"Found {len(text.split())} total words")
+    #print(f"Found {len(text.split())} total words")
     return len(text.split())
 
 def get_num_char(text):
@@ -11,5 +11,16 @@ def get_num_char(text):
             char_dict[char] += 1
         else:
             char_dict[char] = 1
-    print(char_dict)
+    #print(char_dict)
     return char_dict
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
