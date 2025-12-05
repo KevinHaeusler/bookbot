3251219c65d0493450826407264a8e1ed78cabf2
KevinHaeusler <kevin@kh.dev>
2025-12-05 11:35:06 +0100

    CH3L2: Arguments

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_num_char, chars_dict_to_sorted_list
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,12 +7,15 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
-    get_num_char(get_book_text("books/frankenstein.txt"))
-    sorted_list = chars_dict_to_sorted_list(get_num_char(get_book_text("books/frankenstein.txt")))
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    get_num_char(get_book_text(sys.argv[1]))
+    sorted_list = chars_dict_to_sorted_list(get_num_char(get_book_text(sys.argv[1])))
     print("============ BOOKBOT ============")
     print("Analyzing book found at books/frankenstein.txt...")
     print("----------- Word Count ----------")
-    print(f" Found {get_num_words(get_book_text("books/frankenstein.txt"))} total words")
+    print(f" Found {get_num_words(get_book_text(sys.argv[1]))} total words")
     print("--------- Character Count -------")
     for item in sorted_list:
         ch = item["char"]
