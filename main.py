3088ff3d5a2d55826debba12edd650b37495a638
KevinHaeusler <kevin@kh.dev>
2025-12-05 10:47:39 +0100

    L6: Count Characters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_char
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,5 +7,6 @@ def get_book_text(filepath):
 
 def main():
     get_num_words(get_book_text("books/frankenstein.txt"))
+    get_num_char(get_book_text("books/frankenstein.txt"))
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
+import string
+
+char_dict = dict()
+
 def get_num_words(text):
     print(f"Found {len(text.split())} total words")
     return len(text.split())
+
+def get_num_char(text):
+    for char in text.lower():
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    print(char_dict)
